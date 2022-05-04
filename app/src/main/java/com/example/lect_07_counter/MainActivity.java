b97d6c8cf86a8a1f758e64d230dea36978ef695e
Mahreen-Asama <mahreenmehar202@gmail.com>
2022-05-04 11:34:28 +0500

    listeners added

--- app/src/main/java/com/example/lect_07_counter/MainActivity.java
+++ app/src/main/java/com/example/lect_07_counter/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.lect_07_counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    Button btnIncrease,btnDecrease,btnReset;
+    int counter=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview=findViewById(R.id.textView);
+        btnIncrease=findViewById(R.id.btnIncrease);
+        btnDecrease=findViewById(R.id.btnDecrease);
+        btnReset=findViewById(R.id.btnReset);
+
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                setTextView(counter);
+            }
+        });
+        btnDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter--;
+                setTextView(counter);
+            }
+        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter=0;
+                setTextView(counter);
+            }
+        });
+    }
+
+    private void setTextView(int counter) {
+        textview.setText(Integer.toString(counter));
     }
 }
